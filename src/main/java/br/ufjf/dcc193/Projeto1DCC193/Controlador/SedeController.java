b3d93887c3867099a2f09e60fec6ef94fa4212ac
AlineSotte <alinedpaso@gmail.com>
2019-04-23 20:04:12 -0300

    Listar e Adiconar no controler

--- src/main/java/br/ufjf/dcc193/Projeto1DCC193/Controlador/SedeController.java
+++ src/main/java/br/ufjf/dcc193/Projeto1DCC193/Controlador/SedeController.java
@@ -10,46 +10,42 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
-
-
-
 @Controller
- public class SedeController {
+public class SedeController {
 
-@Autowired
-private SedeRepository repsede;   
+    @Autowired
+    private SedeRepository repsede;
 
-@RequestMapping("index.html")
-ModelAndView home(Sede home){
-ModelAndView mv = new ModelAndView();
-mv.setViewName("index");
-mv.addObject("home",home);
-return mv;
-}
-
-@RequestMapping("adicionasede.html")
-    String form(){
-        return "formulariosede";
-    }
-
-@RequestMapping("cadastrarsede.html")
-    ModelAndView novo(Sede s){
-        ModelAndView mv= new ModelAndView();
-        mv.setViewName("cadastrosede");
-        repsede.save(s);
-        mv.addObject("addsede", s);
+    @RequestMapping("index.html")
+    ModelAndView home(Sede home) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("index");
+        mv.addObject("home", home);
         return mv;
     }
 
-@RequestMapping("listasede.html")
-ModelAndView listaSedes(){
-ModelAndView mv=new ModelAndView();
-mv.setViewName("listasede");
-List<Sede> sd = repsede.findAll();
-mv.addObject("listased",sd);
-return mv;
-}
+    @RequestMapping("formulariosede.html")
+    String form() {
+        return "formulariosede";
+    }
 
+    @RequestMapping("addsede.html")
+    ModelAndView novo(Sede s) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("listasede");
+        repsede.save(s);
+        List<Sede> sd = repsede.findAll();
+        mv.addObject("listased", sd);
+        return mv;
+    }
+
+    @RequestMapping("listasede.html")
+    ModelAndView listaSedes() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("listasede");
+        List<Sede> sd = repsede.findAll();
+        mv.addObject("listased", sd);
+        return mv;
+    }
 
 }
